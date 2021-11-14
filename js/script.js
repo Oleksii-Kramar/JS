14a7184add1704ff1d8f9baea43eb88b3d96c9ad
Aleksey <dp020991kos@gmail.com>
2021-11-14 15:21:49 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// alert("HELLO!")
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есь 18?", "18");
+// console.log(typeof(answer));
+
+
+// const answers = [];
+
+// answers [0] = prompt("Как ваше имя?", '');
+// answers [1] = prompt("Как ваша фамилия?", '');
+// answers [2] = prompt("Сколько вам лет?", '');
+
+// console.log(typeof(answers));
+
+
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
